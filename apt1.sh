74e75bf2436c7528128b2319f62943e26f5f610d
FirdausNossro <103670772+YosKL69@users.noreply.github.com>
2022-05-16 06:40:36 +0800

    Update apt1.sh

--- apt1.sh
+++ apt1.sh
@@ -1,20 +1,18 @@
 #!/system/bin/sh
-echo " "
 
 echo -e "\e[1m\e[34mUpdating/upgrading apt and pkg thank you....\e[21m"
  echo " "
-
 echo -e "\e[3m\e[35m made by YosKL69 find me at github\e[21m''
   echo " 
-    
-     echo -e  "\e[3m\e[34mPkg updating......\e[21m""
-        echo " "
-       
-           echo -e "\e[3m\e[34mPkg updating.............."
-               pkg update -y
-               pause 2 
-                 echo " "
-    
+  
+echo -e  "\e[3m\e[34mPkg updating......\e[21m""
+ echo " "
+
+  echo -e "\e[3m\e[34mPkg updating.............."
+  pkg update -y
+  pause 1 
+  echo " "
+ 
 echo -e "\e[3m\e[34m Done next apt...\e[21m"
    apt update -y
    echo " "
@@ -32,7 +30,7 @@ echo -e "\e[3m\e[34mDone pkg upgrade\e[21m"
 echo -e "\e[3m\e[34mNext apt upgrading\e[21m"
   apt upgrade -y
   echo " " 
-   pause 1
+  pause 1
 
 echo -e  "\e[5m\e[31mAll done.Your Pkg and Apt are all update and upgrade. \e[3\e[35Shortcut by YosKL69\e[21" 
 
