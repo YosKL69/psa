590641b9e968db6cc57750b1ce23f867262c7cdc
FirdausNossro <103670772+YosKL69@users.noreply.github.com>
2022-05-16 06:48:58 +0800

    Update apt1.sh
    Jj

--- apt1.sh
+++ apt1.sh
@@ -11,12 +11,11 @@ echo -e "\e[1m\e[34mUpdating/upgrading apt and pkg thank you....\e[21m"
     echo -e "\e[3m\e[34mPkg updating..............\e[21m"
      pkg update -y
      pause 1 
-     echo " "
  
-  echo -e "\e[3m\e[34m Done next apt...\e[21m"
-   apt update -y
-   echo " "
-   pause 1
+echo -e "\e[3m\e[34m Done next apt...\e[21m"
+  apt update -y
+  echo " "
+  pause 1
 
 echo -e "\e[3m\e[34mdone next upgrading...\e[21m""
   pkg upgrade -y
@@ -32,7 +31,7 @@ echo -e "\e[3m\e[34mNext apt upgrading\e[21m"
   echo " " 
   pause 1
 
-echo -e  "\e[5m\e[31mAll done.Your Pkg and Apt are all update and upgrade. \e[3\e[35Shortcut by YosKL69\e[21" 
+echo -e  "\e[5m\e[31mAll done.Your Pkg and Apt are all update and upgrade. Shortcut by YosKL69" 
 
 
   
