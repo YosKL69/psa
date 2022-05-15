aa0b46db7247745f2db9b7490fff2fc500adb876
FirdausNossro <103670772+YosKL69@users.noreply.github.com>
2022-05-16 07:01:18 +0800

    Update apt1.sh

--- apt1.sh
+++ apt1.sh
@@ -1,37 +1,42 @@
 #!/system/bin/sh
-
-echo -e "\e[1m\e[34mUpdating/upgrading apt and pkg thank you....\e[21m"
+ Clear
  echo " "
 
- echo -e "\e[3m\e[35m made by YosKL69 find me at github\e[21m''
+echo -e "\e[1m\e[34mUpdating/upgrading apt and pkg thank you....\e[21m" 
  echo " "
+ 
+echo -e "\e[3m\e[35m made by YosKL69 find me at github\e[21m''
+ echo " "
+ pause 1
   
-   echo -e  "\e[3m\e[34mPkg updating......\e[21m"
-  
-    echo -e "\e[3m\e[34mPkg updating..............\e[21m"
-     pkg update -y
-     pause 1 
+echo -e  "\e[3m\e[34mPkg updating......\e[21m"
+ echo
+ 
+
+echo -e "\e[3m\e[34mPkg updating..............\e[21m"
+ pkg update -y
+ pause 1 
  
 echo -e "\e[3m\e[34m Done next apt...\e[21m"
-  apt update -y
-  echo " "
-  pause 1
+ apt update -y
+ echo " "
+ pause 1
 
 echo -e "\e[3m\e[34mdone next upgrading...\e[21m""
-  pkg upgrade -y
-  echo " "
-    pause 1
+ pkg upgrade -y
+ echo " "
+ pause 1
 
 echo -e "\e[3m\e[34mDone pkg upgrade\e[21m"
-  echo " " 
-  pause 1
+ echo " " 
+ ause 1
 
- echo -e "\e[3m\e[34mNext apt upgrading\e[21m"
-  apt upgrade -y
-  echo " " 
-  pause 1
+echo -e "\e[3m\e[34mNext apt upgrading\e[21m"
+ apt upgrade -y
+ echo " " 
+ pause 1
  
- echo -e  "\e[5m\e[31mAll done.Your Pkg and Apt are all update and upgrade. Shortcut by YosKL69" 
+echo -e  "\e[5m\e[31mAll done.Your Pkg and Apt are all update and upgrade. Shortcut by YosKL69" 
 
 
   
